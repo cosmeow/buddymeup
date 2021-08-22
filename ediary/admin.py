f791014c413ca73b2240c6346df0a5b5573e4ea0
Marina <marina.potuzhnaya@gmail.com>
2021-08-22 18:35:46 +0100

    updated model with users and activities

--- ediary/admin.py
+++ ediary/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import ExerciseType, ExerciseLocation
+from .models import ExerciseLocation
 
 # Register your models here.
-admin.site.register(ExerciseType)
-admin.site.register(ExerciseLocation)
+
--- ediary/models.py
+++ ediary/models.py
@@ -7,27 +7,52 @@ from django.utils import timezone
 # Create your models here.
 class ExerciseLocation(models.Model):
     location = models.CharField(max_length=500)
+    IN = 'IN'
+    OUT = 'OUT'
+    LOCATION_TYPE_CHOICES = (
+                (IN, 'Indoor'),
+                (OUT, 'Outdoor'),
+            )
+    location_type = models.CharField(max_length = 3, choices = LOCATION_TYPE_CHOICES, default = IN)
     def __str__(self):
         return self.location
 
-class ExerciseType(models.Model):
-    type = models.CharField(max_length=500)
-    def __str__(self):
-        return self.type
+class Users(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 254)
+    password = models.CharField(max_length = 10)
+    
 
-class StrengthDiary(models.Model):
-    type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
-    sets = IntegerField()
-    reps = IntegerField()
-    strength_date = models.DateTimeField('strength exercise date')
+class Exercise(models.Model):
+    C = 'C'
+    S = 'S'
+    CATEGORY_CHOICES = [
+                    (C, 'Cardio'),
+                    (S, 'Strength'),
+                ]
+    category = models.CharField(max_length = 1, choices = CATEGORY_CHOICES, default = C)
 
-    def strength_exercise_recently(self):
-        return self.strength_date >= timezone.now() - datetime.timedelta(days=1)
+    RUN = 'RUN'
+    SWIM = 'SWIM'
+    WALK = 'WALK'
+    CYCLE = 'CYCLE'
+    EXERCISE_CHOICES = (
+            (RUN, 'Run'),
+            (SWIM, 'Swim'),
+            (WALK, 'Walk'),
+            (CYCLE, 'Cycle'),
+        )
+    exercise_name = models.CharField(max_length = 5, choices = EXERCISE_CHOICES, default = RUN)
+    
 
-class CardioDiary(models.Model):
-    location = models.ForeignKey(ExerciseLocation, on_delete=models.CASCADE)
-    type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
-    cardio_date = models.DateTimeField('cardio exercise date')
-
+class Activities(models.Model):
+    user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    location = models.ForeignKey(ExerciseLocation, on_delete = models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete = models.CASCADE)
+    date = models.DateTimeField()
+    duration_hrs = models.PositiveIntegerField()
+    duration_min = models.PositiveIntegerField()
+    duration_sec = models.PositiveIntegerField()    
     def cardio_exercise_recently(self):
-        return self.cardio_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
