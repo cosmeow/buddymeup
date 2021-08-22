b1016cf465b2b5eb991b07b2c6e365a11ccdc0dc
Amy <asjacobs006@gmail.com>
2021-08-21 21:28:07 -0800

    playing with models, views and admin

--- ediary/admin.py
+++ ediary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ExerciseType, ExerciseLocation
+
 # Register your models here.
+admin.site.register(ExerciseType)
+admin.site.register(ExerciseLocation)
--- ediary/models.py
+++ ediary/models.py
@@ -1,3 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ExerciseLocation(models.Model):
+    location = models.CharField(max_length=500)
+    def __str__(self):
+        return self.location
+
+class ExerciseType(models.Model):
+    exercise_type = models.CharField(max_length=500)
+    def __str__(self):
+        return self.exercise_type
+
+class ExerciseDiary(models.Model):
+    location = models.ForeignKey(ExerciseLocation, on_delete=models.CASCADE)
+    exercise_type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+    exercise_date = models.DateTimeField('date exercised')
+
+    def exercised_recently(self):
+        return self.exercise_date >= timezone.now() - datetime.timedelta(days=1)
