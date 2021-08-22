1e9692f4dcfd0ba5539346f116a8fbdae4a6dd62
Amy <asjacobs006@gmail.com>
2021-08-22 08:09:43 -0800

    added in strength

--- ediary/models.py
+++ ediary/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.db.models.fields import IntegerField
 from django.utils import timezone
 
 # Create your models here.
@@ -10,14 +11,23 @@ class ExerciseLocation(models.Model):
         return self.location
 
 class ExerciseType(models.Model):
-    exercise_type = models.CharField(max_length=500)
+    type = models.CharField(max_length=500)
     def __str__(self):
-        return self.exercise_type
+        return self.type
 
-class ExerciseDiary(models.Model):
+class StrengthDiary(models.Model):
+    type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+    reps = IntegerField()
+    sets = IntegerField()
+    strength_date = models.DateTimeField('strength exercise date')
+
+    def strength_exercise_recently(self):
+        return self.strength_date >= timezone.now() - datetime.timedelta(days=1)
+
+class CardioDiary(models.Model):
     location = models.ForeignKey(ExerciseLocation, on_delete=models.CASCADE)
-    exercise_type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
-    exercise_date = models.DateTimeField('date exercised')
+    type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+    cardio_date = models.DateTimeField('cardio exercise date')
 
-    def exercised_recently(self):
-        return self.exercise_date >= timezone.now() - datetime.timedelta(days=1)
+    def cardio_exercise_recently(self):
+        return self.cardio_date >= timezone.now() - datetime.timedelta(days=1)
