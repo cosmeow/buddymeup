a46348e33c989683445a93fd5edbbf8a6d5de578
Marina <marina.potuzhnaya@gmail.com>
2021-08-26 22:23:52 +0100

    added home template

--- ediary/urls.py
+++ ediary/urls.py
@@ -6,5 +6,5 @@ app_name = 'ediary'
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/',views.register, name='register'),
-    path('<int:users_id>/', views.home, name='home')
+    path('<int:user_id>/home/', views.home, name='home'),
 ]
