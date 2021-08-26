e82584998c7f86e954553e27088209e099c9c618
Amy <asjacobs006@gmail.com>
2021-08-25 21:14:38 -0800

    adding in templates and static files

--- ediary/urls.py
+++ ediary/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ediary'
 urlpatterns = [
-    path("", views.index, name='index'),
+    path('', views.index, name='index'),
+    path('register/',views.register, name='register'),
     path('<int:users_id>/', views.home, name='home')
 ]
--- ediary/views.py
+++ ediary/views.py
@@ -1,12 +1,16 @@
+from django import template
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import ExerciseLocation, Users, Exercise, Activities
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the exercise diary index.")
+    return render(request, 'ediary/index.html')
 
+def register(request):
+    return render(request, 'ediary/register.html')
 
 def home(request, users_id):
     user = get_object_or_404(Users, pk=users_id)
